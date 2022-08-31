ff19d27f2276a5907b5226bb1dae4d778a26ceed
Eduardo Fernández <feguedi@yahoo.com.mx>
2022-08-30 22:35:39 -0500

    Agregada configuración de ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    eslintPlugin(),
   ],
   server: {
     port: 3030,
   },
-})
+});
